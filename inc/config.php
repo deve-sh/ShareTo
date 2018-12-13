5e4632f7ff981468b82094913393aba11dc32abb
Devesh Kumar <devesh2027@gmail.com>
2018-12-13 15:00:51 +0530

    Removed a small problem.

--- inc/config.php
+++ inc/config.php
@@ -1,13 +1,1 @@
-<?
-
-$host = "localhost";
-$password = "password";
-$user = "userr";
-$dbname = "test";
-$subscript = "shareto_";
-include('inc/connector.php');
-
-$db = new dbdriver();
-
-$db -> connect($host,$user,$password,$dbname);
-?>
+0
